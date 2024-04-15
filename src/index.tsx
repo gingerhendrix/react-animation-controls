a1e6eae9197e3a96ced59134f6410dc76de6261f
Gareth Andrew <gingerhendrix@gmail.com>
2024-04-15 23:01:03 +0100

    Changes from aeons ago

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,12 @@ import 'rc-slider/assets/index.css';
 import { FaPause, FaStepBackward, FaPlay } from 'react-icons/fa';
 
 const inputStyle = {
-  width: '4%',
+  width: '3em',
   backgroundColor: 'white',
   border: '0',
   textAlign: 'center' as 'center',
+  display: 'block',
+  padding: '1px 0px',
 };
 
 interface IconProps {
@@ -41,13 +43,13 @@ const Controls: React.FC<{
   started: boolean,
 }> = ({n, max, started, stop, start, setTick}) => {
   return (
-    <div style={{width: 800, display: 'flex', marginTop: '5px'}}>
+    <div style={{width: '100%', display: 'flex', marginTop: '5px', marginRight: '5px'}}>
       <RestartIcon onClick={() => setTick(0)} />
       { started ? <PauseIcon onClick={stop} /> : <PlayIcon onClick={start} /> }
-      <Slider min={0} max={max} value={n} style={{ flex: '1 1 auto', marginRight: '1em' }} />
-        <input value={n} disabled style={inputStyle} />
-        <span>/</span>
-        <input value={max} disabled style={inputStyle}  />
+      <Slider min={0} max={max} value={n} style={{ flex: '1 1 auto', marginRight: '1em' }} onBeforeChange={stop} onChange={setTick} />
+      <input value={n} style={inputStyle} onChange={e => setTick(Number(e.target.value))} />
+      <div>/</div>
+      <input value={max} disabled style={inputStyle}  />
     </div>
   );
 }
